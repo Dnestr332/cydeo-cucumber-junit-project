73aa15861715abc839d941bdb57e3d58d74b2a51
sashk <sashkovnyc@gmail.com>
2025-05-19 17:12:36 -0400

    Update on project

--- src/test/java/com/cydeo/runners/RunnerTest.java
+++ src/test/java/com/cydeo/runners/RunnerTest.java
@@ -4,11 +4,15 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+import java.util.Locale;
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"pretty", "html:target/cucumber-reports.html"},
         features = "src/test/resources/features",
-        glue = "com.cydeo.step_definitions"
+        glue = "com.cydeo.step_definitions",
+        tags = "@LoginTest",
+        dryRun = false
 )
 public class RunnerTest {
 }
