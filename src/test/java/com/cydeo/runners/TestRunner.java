989c153582a5be80bfec54bb19768ef7dc01a82f
sashk <sashkovnyc@gmail.com>
2025-05-27 16:49:41 -0400

    update

--- src/test/java/com/cydeo/runners/TestRunner.java
+++ src/test/java/com/cydeo/runners/TestRunner.java
@@ -6,9 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty", "html:target/cucumber-reports.html",
-                "html:target/cucumber-report.html",
-                "rerun:target/rerun.txt"
+        plugin = {"pretty",
+                  "html:target/cucumber-reports.html",
+                  "rerun:target/rerun.txt"
         },
         features = "src/test/resources/features",
         glue = "com.cydeo.step_definitions",
